baf6ec2daba5de7e5296dad7aa4442d191e5141b
PeeHaa <info@pieterhordijk.com>
2013-12-27 02:54:16 +0100

    Added hash getter

--- lib/FlareGramster/Storage/Database/Image.php
+++ lib/FlareGramster/Storage/Database/Image.php
@@ -14,13 +14,23 @@ class Image
     public function persistImage(array $data)
     {
         $query = 'INSERT INTO images';
-        $query.= ' (userid, ip, uri, width, height, mime, exif)';
+        $query.= ' (userid, ip, uri, width, height, mime, exif, image)';
         $query.= ' VALUES';
-        $query.= ' (:userid, :ip, :uri, :width, :height, :mime, :exif)';
+        $query.= ' (:userid, :ip, :uri, :width, :height, :mime, :exif, :image)';
 
         $stmt = $this->dbConnection->prepare($query);
         $stmt->execute($data);
 
         return $this->dbConnection->lastInsertId('images_id_seq');
+    }
+
+    public function getHash($id)
+    {
+        $stmt = $this->dbConnection->prepare('SELECT image FROM images WHERE id = :id');
+        $stmt->execute([
+            'id' => $id,
+        ]);
+
+        return $stmt->fetchColumn();
     }
 }
